aa00ed1945a73af2d6a199613fe56e9caf6f663a
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-09-30 14:54:59 +0200

    Added infinite random platform placement

---
+++ JoveJump/Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,79 @@
+﻿// Copyright Code Animo� (Laurens Mathot) � 2020, All rights reserved. Example project for 5th Planet Games
+using UnityEngine;
+
+using System.Collections.Generic;
+
+namespace CodeAnimo
+{
+	public class PlatformManager : MonoBehaviour
+	{
+		public int DesiredPlatformCount = 10;
+		public float MinimumHeightDifference = 5;
+		public float MaximumHeightDifference = 10;
+		public GameObject PlatformPrefab;
+
+		public Camera PlayerCamera;
+
+		private List<GameObject> Platforms;
+
+		private float _nextPlatformHeight;
+
+		private void OnEnable()
+		{
+			if (Platforms == null)
+			{
+				Platforms = new List<GameObject>(DesiredPlatformCount);
+			}
+
+			_nextPlatformHeight = MinimumHeightDifference;
+
+			for(int platformIndex = 0; platformIndex < DesiredPlatformCount; ++platformIndex)
+			{
+				GameObject platform = Instantiate(PlatformPrefab, transform);
+				Platforms.Add(platform);
+
+				PositionPlatform(platform.transform);
+
+				
+			}
+		}
+
+		private void OnDisable()
+		{
+			for (int platformIndex = 0; platformIndex < Platforms.Count; ++platformIndex)
+			{
+				Destroy(Platforms[platformIndex]);
+			}
+			Platforms.Clear();
+		}
+
+
+		private void FixedUpdate()
+		{
+			for (int platformIndex = 0; platformIndex < Platforms.Count; ++platformIndex)
+			{
+				GameObject platform = Platforms[platformIndex];
+				if (PlayerCamera.WorldToViewportPoint(platform.transform.position).y <= 0)
+				{
+					PositionPlatform(platform.transform);
+				}
+			}
+		}
+
+		private void PositionPlatform(Transform platformTransform)
+		{
+			Vector3 position = Vector3.zero;
+			position.x = Random.Range(0.0f, 1.0f);
+			position = PlayerCamera.ViewportToWorldPoint(position);
+			position.y = _nextPlatformHeight;
+			position.z = 0;
+
+			platformTransform.position = position;
+
+			_nextPlatformHeight += Random.Range(MinimumHeightDifference, MaximumHeightDifference);
+		}
+
+
+
+	}
+}
