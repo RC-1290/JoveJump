960144afa77337fa1f29915c4d6c14ebb33ed7ad
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-09-30 18:42:47 +0200

    Made thruster firing more consistent, and reduced the likelyhood of super high jumps by reducing the force and making it an over-time force. It can now only be triggered when not already firing.

--- JoveJump/Assets/Scripts/AutomaticJump.cs
+++ JoveJump/Assets/Scripts/AutomaticJump.cs
@@ -7,48 +7,68 @@ namespace CodeAnimo
 	{
 		[Range(0,1)]
 		public float MaxGroundDistance = 0.1f;
+		public float MaxmimumUpwardsSpeedBeforeLaunch = 0.1f;
 		public LayerMask GroundHitLayers;
 		public Transform GroundDetectionPoint;
 
 		public float TimeBeforeJump = 0.5f;
 		public Vector2 JumpForce;
+		public float ThrustDuration = .034f;
 
 		public float HeightReached = 0;
 
-		public float MaxmimumUpwardsSpeedBeforeLaunch = 0.1f;
-
 		private float _lastTimeAirborne = 0;
 		private Rigidbody2D _ownRigidBody;
+		private bool _performingThrust = false;
+		private float _thrustStartTime = 0;
 
 		private void OnEnable()
 		{
 			_ownRigidBody = GetComponent<Rigidbody2D>();
 			HeightReached = 0;
 			_lastTimeAirborne = 0;
+			_performingThrust = false;
 		}
 
 		private void FixedUpdate()
 		{
-			RaycastHit2D hit = Physics2D.Raycast(GroundDetectionPoint.position, Vector2.down, MaxGroundDistance, GroundHitLayers.value);
-
-			bool notGoingUp = _ownRigidBody.velocity.y <= MaxmimumUpwardsSpeedBeforeLaunch;
-
-			if (hit.collider != null && notGoingUp)
+			if (!_performingThrust)
 			{
-				float platformHeight = hit.point.y;
-				if (platformHeight > HeightReached)
-				{
-					HeightReached = platformHeight;
-				}
+				// Check if we've landed:
+				RaycastHit2D hit = Physics2D.Raycast(GroundDetectionPoint.position, Vector2.down, MaxGroundDistance, GroundHitLayers.value);
+				bool weHitSomething = hit.collider != null;
+				bool notGoingUp = _ownRigidBody.velocity.y <= MaxmimumUpwardsSpeedBeforeLaunch;
 
-				if (Time.fixedTime - _lastTimeAirborne > TimeBeforeJump)
+				if (weHitSomething && notGoingUp)
 				{
-					_ownRigidBody.AddForce(JumpForce);
+					float platformHeight = hit.point.y;
+					if (platformHeight > HeightReached)
+					{
+						HeightReached = platformHeight;
+					}
+
+					if (Time.fixedTime - _lastTimeAirborne > TimeBeforeJump)
+					{
+						_performingThrust = true;
+						_thrustStartTime = Time.fixedTime;
+					}
+				}
+				else
+				{
+					_lastTimeAirborne = Time.fixedTime;
 				}
 			}
-			else
+
+			if (_performingThrust)
 			{
-				_lastTimeAirborne = Time.fixedTime;
+				if (Time.fixedTime < _thrustStartTime + ThrustDuration)
+				{
+					_ownRigidBody.AddForce(JumpForce, ForceMode2D.Force);
+				}
+				else
+				{
+					_performingThrust = false;
+				}
 			}
 		}
 
