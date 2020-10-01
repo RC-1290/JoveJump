6fa4fd48da2514e2ca3252b4d831c62c73fe2abf
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-10-01 15:25:20 +0200

    The arms of the main character will attempt to hold a pose, instead of going into a t-pose.

---
+++ JoveJump/Assets/Scripts/Aesthetics/HoldPose.cs
@@ -0,0 +1,35 @@
+﻿// Copyright Code Animo� (Laurens Mathot) � 2020, All rights reserved. Example project for 5th Planet Games
+using UnityEngine;
+
+namespace CodeAnimo
+{
+	public class HoldPose : MonoBehaviour
+	{
+		public HingeJoint2D PosedJoint;
+
+		public float DesiredAngle = 45;
+		public float MotorSpeed = 100;
+
+		private void Reset()
+		{
+			PosedJoint = GetComponent<HingeJoint2D>();
+		}
+
+
+		void Update()
+		{
+			var motor = PosedJoint.motor;
+
+			if (PosedJoint.jointAngle < DesiredAngle)
+			{
+				motor.motorSpeed = MotorSpeed;
+			}
+			else if (PosedJoint.jointAngle > DesiredAngle)
+			{
+				motor.motorSpeed = -MotorSpeed;
+			}
+			PosedJoint.motor = motor;
+			
+		}
+	}
+}
